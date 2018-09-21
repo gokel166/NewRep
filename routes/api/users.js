7e9872d81f8895bb1d0ff2ad345b6e0af0fc3428
gokel166 <okellogeorge40@yahoo.com>
2018-09-20 20:41:09 -0500

    create post routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const router = express.Router();
 
+// Load user model
+const User = require('../../models/User');
+
+// get api/users/test
+// access --- public
 router.get('/test', (req, res) => {
   res.json({ msg: 'User works' });
+});
+
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (user) {
+        return res.status(400).json({ email: 'Email already exists' });
+      }
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser.save().then(user => res.json(user)).catch(err => console.log(err));
+        });
+      });
+    });
 });
 
 module.exports = router;
